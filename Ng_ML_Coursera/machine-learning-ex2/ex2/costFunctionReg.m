2b0245538132d2cc14f91e2281291312acdf0b51
Darren Tanner <34904658+dstanner@users.noreply.github.com>
2018-09-02 12:25:57 -0700

    Initial commit

---
+++ Ng_ML_Coursera/machine-learning-ex2/ex2/costFunctionReg.m
@@ -0,0 +1,34 @@
+function [J, grad] = costFunctionReg(theta, X, y, lambda)
+%COSTFUNCTIONREG Compute cost and gradient for logistic regression with regularization
+%   J = COSTFUNCTIONREG(theta, X, y, lambda) computes the cost of using
+%   theta as the parameter for regularized logistic regression and the
+%   gradient of the cost w.r.t. to the parameters. 
+
+% Initialize some useful values
+m = length(y); % number of training examples
+
+% You need to return the following variables correctly 
+J = 0;
+grad = zeros(size(theta));
+
+% ====================== YOUR CODE HERE ======================
+% Instructions: Compute the cost of a particular choice of theta.
+%               You should set J to the cost.
+%               Compute the partial derivatives and set grad to the partial
+%               derivatives of the cost w.r.t. each parameter in theta
+
+logits = X*theta;
+preds = zeros(length(y),1);
+for i = 1:length(y)
+    preds(i) = 1/(1 + exp(-logits(i)));
+end
+
+J = (1/m)*((-y'*log(preds)) - (1-y)'*log(1-preds)) + (lambda/(2*m))*sum(theta(2:end).^2);
+
+
+grad(1) = (1/m).*(X(:,1)'*(preds - y));
+grad(2:end) = (1/m)*X(:,2:end)'*(preds - y) + (lambda/m).*theta(2:end);
+
+% =============================================================
+
+end
