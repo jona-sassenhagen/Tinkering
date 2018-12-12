4d187d249373b974434a05814bd7463a1d26ec55
Darren Tanner <34904658+dstanner@users.noreply.github.com>
2018-12-11 17:17:21 -0800

    Fix MSE

--- code_examples/regression.py
+++ code_examples/regression.py
@@ -1,6 +1,8 @@
 import pandas as pd
 import numpy as np
 import seaborn as sns
+import statsmodels.api as sm
+import statsmodels.formula.api as smf
 
 pd.options.display.html.table_schema = True
 pd.options.display.max_rows = None
@@ -8,9 +10,10 @@ pd.options.display.max_rows = None
 tips = sns.load_dataset("tips")
 y = tips['tip']
 X = tips.drop('tip', axis=1)
+X = pd.get_dummies(X, drop_first=True)
+new_X = statsmodels.tools.add_constant(X)
 
-null_mse = sum(y**2)/(2*X.shape[1])
-null_mse
+
 
 def mse(X, y, coefs):
     '''
@@ -21,13 +24,22 @@ def mse(X, y, coefs):
 
     # Predicted value is X matrix times coefs vector
     preds = X @ coefs
-    sum_sqr_resid = np.sum(np.square(np.subtract(preds, y)))
-    mse = sum_sqr_resid/(2*n)
+    resid = preds - y
+    sse = np.square(resid).sum()
+    mse = sse/n
 
     return mse
+
 
 def log_loss(X, y, coefs):
     '''
     Compute the log loss function for logisttic regression,
     given an X matrix, y vector, and vector of coefficients.
     '''
+    n = X.shape[0]
+    logits = X @ coefs
+    sigmoid = lambda x: 1/(1 + np.exp(-x))
+    preds = sigmoid(logits)
+    loss_vec = -1*(y.T*np.log(preds) + (1-y).T*np.log(1-preds))
+
+    return loss_vec.sum()/n
