e9f187713d1df29cfa7547e31949e3646177479c
Darren Tanner <34904658+dstanner@users.noreply.github.com>
2018-12-11 18:33:14 -0800

    Update formatting, log_loss

--- code_examples/regression.py
+++ code_examples/regression.py
@@ -10,9 +10,6 @@ pd.options.display.max_rows = None
 tips = sns.load_dataset("tips")
 y = tips['tip']
 X = tips.drop('tip', axis=1)
-X = pd.get_dummies(X, drop_first=True)
-new_X = statsmodels.tools.add_constant(X)
-
 
 
 def mse(X, y, coefs):
@@ -36,10 +33,12 @@ def log_loss(X, y, coefs):
     Compute the log loss function for logisttic regression,
     given an X matrix, y vector, and vector of coefficients.
     '''
+    def sigmoid(x):
+        1/(1 + np.exp(-x))
+
     n = X.shape[0]
     logits = X @ coefs
-    sigmoid = lambda x: 1/(1 + np.exp(-x))
     preds = sigmoid(logits)
-    loss_vec = -1*(y.T*np.log(preds) + (1-y).T*np.log(1-preds))
+    loss_sum = -1*(y.T*np.log(preds) + (1-y).T*np.log(1-preds))
 
-    return loss_vec.sum()/n
+    return loss_sum/n
